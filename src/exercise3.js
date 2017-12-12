865d6a0b6802291cd72391353fa9edd27d515a1e
jonathantouchard <johnn49@hotmail.fr>
2017-12-12 16:32:45 +0100

    Exercise 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,3 +1,17 @@
 exports.run = function(input) {
-  console.log("Exercise #3");
+
+  nb = 0;
+
+  for(var i = 0; i < input.length; i++){
+
+    if(input[i].operator === 'add'){
+      nb += input[i].value;
+    }
+    else if(input[i].operator === 'substract'){
+      nb -= input[i].value;
+    }
+  }
+
+  return nb;
+
 };
